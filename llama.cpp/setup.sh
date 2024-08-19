a3568d74cc9b6f60ff5327413381dbef0f538a8c
Jan Grzybek <jan@amperecomputing.com>
2024-08-19 15:37:40 +0200

    Update setup.sh

--- llama.cpp/setup.sh
+++ llama.cpp/setup.sh
@@ -1,5 +1,5 @@
 set -eo pipefail
 
 apt update && apt install -y docker.io
-apt-get update && apt-get install -y python3 python3-pip
+apt-get update && apt-get install -y python3 python3-pip lm-sensors
 pip3 install huggingface-hub psutil
